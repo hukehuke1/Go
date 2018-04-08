047b2be2827b1c58b1ed654c70e249e91f07f5b2
exHooker <hukehuke@sina.com>
2018-04-08 16:49:56 +0800

    update

--- go.go
+++ go.go
@@ -3,6 +3,7 @@
 package main
 // 导入其他包
 import "fmt"
+// import "unsafe"
 // 由main函数作为程序入口点启动
 func main1(){
 	// 一般类型声明
@@ -44,6 +45,36 @@ func main3(){
 }
 
 
+func main4(){
+	/* const(
+		a = "abc"
+		b = len(a)
+		c = unsafe.Sizeof(a)
+	)
+	println(a,b,c) */
+    const (
+		a = iota   //0
+		b          //1
+		c          //2
+		d = "ha"   //独立值，iota += 1
+		e          //"ha"   iota += 1
+		f = 100    //iota +=1
+		g          //100  iota +=1
+		h = iota   //7,恢复计数
+		i          //8
+	)
+	fmt.Println(a,b,c,d,e,f,g,h,i)
+}
+
 func main(){
-	
+	const(
+		i = 1 <<iota
+		j = 3 <<iota
+		k
+		l
+	)
+    fmt.Println("i=",i)
+    fmt.Println("j=",j)
+    fmt.Println("k=",k)
+    fmt.Println("l=",l)
 }
